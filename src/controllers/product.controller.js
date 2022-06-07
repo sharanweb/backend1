6c5d4a89fb89215cf40d9a11e004efbff2ce8e2a
Sharan Dooganavar <91963829+sharanweb@users.noreply.github.com>
2022-06-07 20:20:29 +0530

    Update product.controller.js

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -3,86 +3,56 @@ const router = express.Router();
 
 const Product = require("../models/product.model");
 
-router.get("", async(req,res)=>{
+
+
+router.get('/',async(req,res)=>{
     try {
-        const product = await Product.find().lean().exec();
+        let page = req.query.page ||1
+        let pagesize = req.query.pagesize||5
+        let filter = req.query.filter
+        let sort = req.query.sort
+        const skip=(page-1)*pagesize;
+        if(filter!=="all"){
+        const product = await Product
+          .find({brand:{$eq:filter}})
+          .skip(skip).limit(pagesize)
+          .sort({price:sort})
+          .lean()
+          .exec()
+        const total_pages=Math.ceil((await Product.find({brand:{$eq:filter}}).countDocuments())/pagesize)
+        return res.send({total_pages,product})
+        }
+        else{
+            const product = await Product
+              .find()
+              .skip(skip)
+              .limit(pagesize)
+              .sort({price:sort})
+              .lean()
+              .exec()
+            const total_pages = Math.ceil((await Product.find().sort({price:sort}).countDocuments())/pagesize)
+          
+            return res.send({total_pages,product})
+           
+        }
+    } 
+    catch (error) {
+        res.send(error)
+    }
+})
+router.get("/listing", async(req,res)=>{
+    try {
+        const page = req.query.page || 1;
+        const pagesize = req.query.pagesize || 10; 
+        const skip = (page - 1) * pagesize;
+        const product = await Product.find().skip(skip).limit(pagesize).lean().exec();
+        const totalPages = Math.ceil((await Product.find().countDocuments()))
         return res.status(201).send({product:product});
     } catch (error) {
         return res.status(500).send({message: error.message});
     }
 });
 
-router.post("/",async(req,res)=>{
-    try {
-        const item = await Product.create(req.body);
-        return res.status(201).send(item);
-    } catch (error) {
-        return res.status(500).send({message: error.message});
-    }
-});
 
-//single products
-router.get("/:id", async (req, res) => {
-    const product = await Product
-        .findById(req.params.id)
-        .lean()
-        .exec()
-    return res.status(201).send({ product })
-})
-
-// fabric 
-router.get("/sort/fabric", async (req, res) => {
-    const fabric = req.query.fabric;
-    const product = await Product
-        .find({ gender: { $eq: `${fabric}` } })
-        .lean()
-        .exec();
-    return res.status(201).send({ product:product });
-})
-
-//colors
-router.get("/sort/color", async (req, res) => {
-    const color = req.query.color;
-    const prodColor = await Product
-        .find({ color: { $eq: `${color}` } })
-        .lean()
-        .exec();
-    return res.status(201).send({ product:prodColor });
-})
-
-// brands
-router.get("/sort/brand", async (req, res) => {
-    try {
-        const brand = req.query.brand;
-    const prodBrand = await Product
-        .find({ brand: { $eq: `${brand}` } })
-        .lean()
-        .exec();
-    return res.status(200).send({ product:prodBrand });
-    } catch (error) {
-        return res.status(500).send({message: error.message});
-    }
-});
-
-//types
-router.get("/sort/type", async (req, res) => {
-    const type = req.query.type;
-    const prodBrand = await Product
-        .find({ type: { $eq: `${type}` } })
-        .lean()
-        .exec();
-    return res.status(201).send({ product:prodBrand });
-})
-
-// price
-router.get("/sort/price", async (req, res) => {
-    const from = req.query.from;
-    const to = req.query.to;
-    const prodPrice = await Product
-        .find({ $and: [{ price: { $gt: `${from}` } }, { price: { $lt: `${to}` } }] })
-        .lean()
-        .exec();
-    return res.status(201).send({ product:prodPrice });
-})
 
 module.exports = router;
